51a047db09e02959cc7c32982d12c4628e4376e9
Daniel Choi <dhchoi@gmail.com>
2011-01-13 21:49:36 -0500

    refactoring to be tool or module

--- lib/feed_yamlizer.rb
+++ lib/feed_yamlizer.rb
@@ -31,7 +31,6 @@ class FeedYamlizer
 
   def add_feed_metaresult
     fields = [:title, :link, :xml_encoding]
-    @xml_encoding = @feed[:xml_encoding]
     @result[:meta] = fields.reduce({}) {|memo, field| 
       memo[field] = @feed[field]
       memo
@@ -64,16 +63,43 @@ class FeedYamlizer
     @result[:items][-1][:content][:text] = textified
   end
 
-  def self.run(feed_xml)
-    check_for_tidy
-    parsed_data = FeedYamlizer::FeedParser.new(feed_xml).result
-    result = FeedYamlizer.new(parsed_data).result
-    result
-  end
+  class << self
+    def xml_encoding(rawxml)
+      x = rawxml.scan(/encoding=["']([^"']+)["']/)
+      encoding = x && x[0] && x[0][0]
+      STDERR.puts "xml encoding: #{encoding.inspect}"
+      encoding
+    end
 
-  def self.check_for_tidy
-    if `which tidy` == ''
-      abort "Please install tidy"
+    def to_utf(x, encoding = 'ISO-8859-1') 
+      x = Iconv.conv("UTF-8//TRANSLIT//IGNORE", encoding, x)
+    end
+
+    def check_for_tidy
+      if `which tidy` == ''
+        abort "Please install tidy"
+      end
+    end
+
+    def run(feed_xml, encoding)
+      check_for_tidy
+      feed_xml = to_utf feed_xml, encoding
+      parsed_data = FeedYamlizer::FeedParser.new(feed_xml).result
+      result = FeedYamlizer.new(parsed_data).result
+      result
+    end
+
+    def process_xml(xml)
+      run xml, xml_encoding(xml)
+    end
+
+    def process_url(url)
+      response = open(url)
+      charset = response.charset
+      #STDERR.puts "charset: #{charset}"
+      xml = response.read
+      encoding = charset || xml_encoding(xml) || "ISO-8859-1"
+      run xml, encoding
     end
   end
 end
